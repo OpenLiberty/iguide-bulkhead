e2bc176ac2cc996518672721d7013b3ecec0c758
Lavena Chan <lavena@us.ibm.com>
2018-12-13 13:21:37 -0600

    Fix sample to use virtualFinancialAdvisor url, change bulkhead value and waitqueue to 2, add rest method to get result of waiting queue, remove restriction to restart server

--- finish/src/main/java/io/openliberty/guides/bulkhead/global/eBank/rest/Producer.java
+++ finish/src/main/java/io/openliberty/guides/bulkhead/global/eBank/rest/Producer.java
@@ -13,6 +13,7 @@ package io.openliberty.guides.bulkhead.global.eBank.rest;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -22,63 +23,70 @@ import io.openliberty.guides.bulkhead.global.eBank.microservices.Service;
 import io.openliberty.guides.bulkhead.global.eBank.microservices.Utils;
 import io.openliberty.guides.bulkhead.global.eBank.microservices.BankService;
 
-@Path("vfa")
+@Path("virtualFinancialAdvisor")
 public class Producer {
 
-    // for demo purpose only to keep track
-    // of the number in the waiting queue
-    private static int waitQueue = 0;
+    private static int value = BankService.bulkheadValue;
+    private static int waitingTaskQueue = BankService.bulkheadWaitingQueue;
     private static int requests = 0; 
+    private static Future[] waitingInQueueFuture = new Future[waitingTaskQueue];
 
     @Inject
     BankService bankService;
 
     @GET
+    @Path("/vfa")
     @Produces(MediaType.TEXT_HTML)
     public String getVFA() {
-        int value = bankService.bulkheadValue;
-        int waitingTaskQueue = bankService.bulkheadWaitingQueue;
-        int localWaitQueue = waitQueue;  // Local copy of the waitQueue value
-
         String returnMsg = "";        
             
         try {
-            // once the scheduling is displayed 
-            // return right away to stop the simulation
-            // until the server is restart
-            if (requests > value && waitQueue > waitingTaskQueue) {
-                returnMsg = Utils.getHTMLRestart();
-                return returnMsg;
-            }
-
+            int localRequestNum =  adjustRequestCount(true);
             Future<Service> future = bankService.requestForVFA();
-            requests++;
-            
-            if (requests > value) {
-                waitQueue++;
-                localWaitQueue = waitQueue;                
-            }
         
-            if (requests > value &&
-                localWaitQueue <= waitingTaskQueue) {
-                // for the purpose of demo
-                // since we are keeping track of the waitQueue
-                // put the same amount of a sleep here as in the microservice
-                // to simulate the waiting
-                Thread.sleep(bankService.TIMEOUT);
-                //There are no financial advisors available. You are number # in the queue               
-                returnMsg = Utils.getHTMLForWaitingQueue(localWaitQueue);
+            if (localRequestNum > value && localRequestNum <= value + waitingTaskQueue) {
+                int futureNumber = localRequestNum - value - 1;
+                waitingInQueueFuture[futureNumber] = future;
+                returnMsg = Utils.getHTMLForWaitingQueue(localRequestNum-value);
                 return returnMsg;
             }
                 
             // You are talking to advisor #
             Service service = future.get();
             returnMsg = service.toString();
-      
         } catch (Exception e) {
             returnMsg = e.getMessage();
         } 
         return returnMsg;
     }
     
+    @GET
+    @Path("/getResultInQueue/{numInQueue}")
+    @Produces(MediaType.TEXT_HTML)
+    public String getResutInQueue(@PathParam("numInQueue") int numInQueue) {
+        if (numInQueue - 1 >= waitingTaskQueue) {
+            return "<h3>Unexpected error</h3>";
+        }
+        Future<Service> future = waitingInQueueFuture[numInQueue-1];
+        String returnString = "";
+        waitingInQueueFuture[numInQueue-1] = null;
+        try {
+            if (future != null) {
+                Service service = future.get();
+                returnString = service.toString();
+            }
+        } catch (Exception ex) {
+            returnString = "<div>Chat has canceled</div>";
+        } 
+        return returnString;
+    }
+
+    public static synchronized int adjustRequestCount(boolean add) {
+        if (add) {
+            requests++;
+        } else {
+            requests--;
+        }
+        return requests;
+    }
 }
